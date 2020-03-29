4f16de94c080fefa24dd11e974e8d6c5c1280ca2
Melanie Fuchs <mele.fuchs@gmx.ch>
2020-03-29 11:48:29 +0200

    init project

---
+++ src/main/java/ch/yoursource/causationfinder/CausationfinderApplication.java
@@ -0,0 +1,13 @@
+package ch.yoursource.causationfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CausationfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CausationfinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/yoursource/causationfinder/CausationfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.yoursource.causationfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CausationfinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

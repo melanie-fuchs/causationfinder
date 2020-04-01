733620f1cb9bfaa6ca2e744b0a54f8f59b3c18aa
Melanie Fuchs <mele.fuchs@gmx.ch>
2020-04-01 11:53:06 +0200

    .

--- src/main/java/ch/yoursource/causationfinder/registration/controller/RegistrationController.java
+++ src/main/java/ch/yoursource/causationfinder/registration/controller/RegistrationController.java
@@ -1,8 +1,12 @@
 package ch.yoursource.causationfinder.registration.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.context.request.WebRequest;
 
 import ch.yoursource.causationfinder.registration.dto.UserRegistrationDto;
@@ -13,8 +17,14 @@ public class RegistrationController {
 		@GetMapping("/registration") // comes from our configuration file (DemoSecurityConfiguration)
 		public String showRegistrationForm(WebRequest request, Model model) {
 			UserRegistrationDto userRegistrationDto = new UserRegistrationDto();
-			model.addAttribute("user", userRegistrationDto);
+			model.addAttribute("userRegistrationDto", userRegistrationDto);
 			
-			return "registration";
+			return "user-registration/user-registration-form";
+		}
+		
+
+		@PostMapping("/registration")
+		public String saveUser(@ModelAttribute("userRegistrationDto") @Valid UserRegistrationDto userRegistrationDto) {
+			return "";
 		}
 }

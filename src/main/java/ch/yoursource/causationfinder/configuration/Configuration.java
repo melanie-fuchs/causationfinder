6ad259558a731315a363ef4eb41fefd3907ad05f
Melanie Fuchs <mele.fuchs@gmx.ch>
2020-04-01 11:52:00 +0200

    .

---
+++ src/main/java/ch/yoursource/causationfinder/configuration/Configuration.java
@@ -0,0 +1,15 @@
+package ch.yoursource.causationfinder.configuration;
+
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class Configuration extends WebSecurityConfigurerAdapter {
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+	    web.ignoring().antMatchers("/registration");
+	}
+	
+}

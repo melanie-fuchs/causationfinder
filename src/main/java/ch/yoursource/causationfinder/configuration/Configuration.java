cabeded37b6433f637cc67af0c5df6ba356aa7d6
Melanie Fuchs <mele.fuchs@gmx.ch>
2020-04-02 13:11:57 +0200

    configured basic internationalization

--- src/main/java/ch/yoursource/causationfinder/configuration/Configuration.java
+++ src/main/java/ch/yoursource/causationfinder/configuration/Configuration.java
@@ -1,12 +1,24 @@
 package ch.yoursource.causationfinder.configuration;
 
+import java.util.Locale;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @EnableWebSecurity
 public class Configuration extends WebSecurityConfigurerAdapter {
 
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+		sessionLocaleResolver.setDefaultLocale(Locale.US);
+		return sessionLocaleResolver;
+	}
+	
 	@Override
 	public void configure(WebSecurity web) throws Exception {
 	    web.ignoring().antMatchers("/registration");

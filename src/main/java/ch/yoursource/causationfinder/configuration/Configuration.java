2bbb1d7f131ad3d03b7407b3238a7aeab755b432
Melanie Fuchs <mele.fuchs@gmx.ch>
2020-04-07 11:17:04 +0200

    .

--- src/main/java/ch/yoursource/causationfinder/configuration/Configuration.java
+++ src/main/java/ch/yoursource/causationfinder/configuration/Configuration.java
@@ -5,6 +5,7 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -33,15 +34,25 @@ public class Configuration extends WebSecurityConfigurerAdapter implements WebMv
 		return bCryptPasswordEncoder;
 	}
 
-//	TODO implement to allow multiple languages
-//	//default locale set to US (if key not found in messages_XX.properties-files,
-//	// messages.properties will be used as fallback
+	// TODO implement to allow multiple languages
+	//default locale set to US (if key not found in messages_XX.properties-files,
+	// messages.properties will be used as fallback
 //	@Bean
 //	public LocaleResolver localeResolver() {
 //		SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
 //		sessionLocaleResolver.setDefaultLocale(Locale.US);
 //		return sessionLocaleResolver;
 //	}
+	
+
+//	
+//	@Bean
+//	public LocalValidatorFactoryBean getValidator() {
+//	    LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+//	    bean.setValidationMessageSource(messageSource());
+//	    return bean;
+//	}
+//	
 //	
 //	// interceptor will switch to a new locale based on value of the lang-parameter appended to a request
 //	@Bean
@@ -61,8 +72,20 @@ public class Configuration extends WebSecurityConfigurerAdapter implements WebMv
 	@Override
 	public void configure(WebSecurity web) throws Exception {
 	    web.ignoring()
-	    .antMatchers("/registration")
-	    .antMatchers("/");
+	    .antMatchers("/registration", "/");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests(). // restict access based on the HttpServletRequest
+		anyRequest().authenticated() // any request: the user MUST be logged in! (authenticated)
+		.and()
+		.formLogin()
+		.defaultSuccessUrl("/userhome", false);
+		//.loginProcessingUrl("/authenticateTheUser") // login form should POST data to this url for processing (check user IF and PW) --> NO CONTROLLER REQUEST MAPPING REQUIRED FOR THIS! SPRING MAGIC! But the name must be exactly written like that! /authenticateTheUser
+		//.permitAll() // makes that everyone can see this login page (its not hidden to anyone)
+		//.and()
+		//.logout().permitAll(); // add logout support for default URL /logout
 	}
 	
 }

f6dfc528c8b89d6785ad80232b480f93c1a904f8
Melanie Fuchs <mele.fuchs@gmx.ch>
2020-04-07 18:26:31 +0200

    creating page, controller and dto for updating a user

---
+++ src/main/java/ch/yoursource/causationfinder/controller/UpdateUserController.java
@@ -0,0 +1,93 @@
+package ch.yoursource.causationfinder.controller;
+
+import javax.validation.Valid;
+
+import org.apache.tomcat.util.buf.UDecoder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.ModelAndView;
+
+import ch.yoursource.causationfinder.dto.UpdateUserDto;
+import ch.yoursource.causationfinder.entity.User;
+import ch.yoursource.causationfinder.service.UserService;
+
+@Controller
+public class UpdateUserController {
+
+    private UserService userService;
+
+    @Autowired
+    public UpdateUserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/updateuser")
+    public String showUpdateUserForm(WebRequest request, Model model) {
+
+        Authentication loggedInUser = SecurityContextHolder.getContext().getAuthentication();
+        String username = loggedInUser.getName();
+
+        User user = userService.findByUsername(username);
+        UpdateUserDto updateUserDto = new UpdateUserDto(user);
+
+        model.addAttribute("updateUserDto", updateUserDto);
+
+        return "user-profile/updateuserform";
+    }
+
+    @PostMapping("/updateuser")
+    public ModelAndView saveUpdate(
+            @ModelAttribute("updateUserDto") @Valid UpdateUserDto updatedUserData, 
+            BindingResult result,
+            WebRequest request,
+            Errors errors) {
+
+        boolean hasErrors = false;
+
+        if (result.hasErrors()) {
+            hasErrors = true;
+        }
+        
+        Authentication loggedInUser = SecurityContextHolder.getContext().getAuthentication();
+        String username = loggedInUser.getName();
+
+        User oldUserData = userService.findByUsername(username);
+
+        // get user with e-mail that the user entered in updateform. Might be null
+        User existingUserByEmail = userService.findByEmail(updatedUserData.getEmail());
+        // get user with username that the user entered in updateform. Might be null
+        User existingUserByUsername = userService.findByUsername(updatedUserData.getUsername());
+
+        
+        if (existingUserByEmail != null
+                && oldUserData.getId() != existingUserByEmail.getId()) {
+            errors.rejectValue("email", "messages.updateuser.emailAlreadyExists");
+            hasErrors = true;
+        }
+        
+        if (existingUserByUsername != null
+                && oldUserData.getId() != existingUserByUsername.getId()) {
+            errors.rejectValue("username", "messages.updateuser.usernameAlreadyExists");
+            hasErrors = true;
+        }
+        
+        if (hasErrors) {
+            System.out.println("\t>>> WITHIN UpdateUserController: " + result.getFieldErrors().toString());
+            // first parameter is path to registration-form, second parameter is variable-name to access dto in template/html file)
+            return new ModelAndView("user-profile/updateuserform", "updateUserDto", updatedUserData);
+        }
+        
+        
+        userService.saveUpdatedUserData(oldUserData, updatedUserData);
+        return new ModelAndView("home/userhome");
+    }
+}
---
+++ src/main/java/ch/yoursource/causationfinder/dto/UpdateUserDto.java
@@ -0,0 +1,84 @@
+package ch.yoursource.causationfinder.dto;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import ch.yoursource.causationfinder.entity.User;
+
+public class UpdateUserDto {
+
+    @NotEmpty
+    @NotNull
+    @Size(min = 3, max = 20)
+    private String username;
+
+    private String firstName;
+
+    private String lastName;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    private Date birthdate;
+
+    @NotNull
+    @NotEmpty
+    @Email
+    private String email;
+
+    public UpdateUserDto() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public UpdateUserDto(User user) {
+        this.username = user.getUsername();
+        this.firstName = user.getFirstName();
+        this.lastName = user.getLastName();
+        this.birthdate = user.getBirthdate();
+        this.email = user.getEmail();
+    }
+}

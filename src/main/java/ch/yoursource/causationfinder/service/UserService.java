61adb15c4a6830331dbc545333fe2c0f188b5d7d
Melanie Fuchs <mele.fuchs@gmx.ch>
2020-04-07 18:28:36 +0200

    adding methods so part of userdata can be edited

--- src/main/java/ch/yoursource/causationfinder/service/UserService.java
+++ src/main/java/ch/yoursource/causationfinder/service/UserService.java
@@ -1,10 +1,13 @@
 package ch.yoursource.causationfinder.service;
 
+import ch.yoursource.causationfinder.dto.UpdateUserDto;
 import ch.yoursource.causationfinder.entity.User;
 
 public interface UserService {
 	
 	void save(User user);
+	void saveUpdatedUserData(User currentUser, UpdateUserDto updateUserDto);
+	void saveChangedPassword(User currentUser, String newPassword);
 	User findByUsername(String username);
 	User findByEmail(String email);
 }

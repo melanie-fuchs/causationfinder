f2502b3142493a12f9dac552460a4bb16ffe2513
Melanie Fuchs <mele.fuchs@gmx.ch>
2020-04-09 16:12:32 +0200

    create repository to get a list of all Preset Parameters in the CustomParameterTable

--- src/main/java/ch/yoursource/causationfinder/repository/CustomParameterRepository.java
+++ src/main/java/ch/yoursource/causationfinder/repository/CustomParameterRepository.java
@@ -1,9 +1,18 @@
 package ch.yoursource.causationfinder.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import ch.yoursource.causationfinder.entity.CustomParameter;
+import ch.yoursource.causationfinder.entity.User;
 
 public interface CustomParameterRepository extends JpaRepository<CustomParameter, Integer> {
 
+    // find all predefinedParameters per User
+    @Query("SELECT c FROM CustomParameter c WHERE c.user = :user AND c.predefinedParam IS NOT NULL")
+    public List<CustomParameter> findPredefinedByUser(@Param("user") User user);
+    
 }

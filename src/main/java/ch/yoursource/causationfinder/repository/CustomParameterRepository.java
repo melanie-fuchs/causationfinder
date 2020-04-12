a7efec2488170396aa872aa2e14fb7eac3e6d916
Melanie Fuchs <mele.fuchs@gmx.ch>
2020-04-12 20:17:02 +0200

    new method and query to get all customParameters by a given user and type

--- src/main/java/ch/yoursource/causationfinder/repository/CustomParameterRepository.java
+++ src/main/java/ch/yoursource/causationfinder/repository/CustomParameterRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.repository.query.Param;
 
 import ch.yoursource.causationfinder.entity.CustomParameter;
 import ch.yoursource.causationfinder.entity.User;
+import ch.yoursource.causationfinder.setup.ParameterType;
 
 public interface CustomParameterRepository extends JpaRepository<CustomParameter, Integer> {
 
@@ -19,4 +20,8 @@ public interface CustomParameterRepository extends JpaRepository<CustomParameter
     @Query("SELECT c FROM CustomParameter c WHERE c.user = :user AND c.predefinedParam IS NULL")
     public List<CustomParameter> findCustomByUser(@Param("user") User user);
     
+    // find all active customParameters per user 
+    @Query("SELECT c FROM CustomParameter c WHERE c.user = :user AND c.active=true AND c.type=:type")
+    public List<CustomParameter> findActiveByUserAndType(@Param("user") User user, @Param("type") ParameterType type);   
 }
+ 

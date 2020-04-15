b91e76420ea6232df8ecf14edff3709f0cab85b3
Melanie Fuchs <mele.fuchs@gmx.ch>
2020-04-15 10:37:10 +0200

    Query to get the observedDayValues from a given date-range

--- src/main/java/ch/yoursource/causationfinder/repository/ObservedDayValueRepository.java
+++ src/main/java/ch/yoursource/causationfinder/repository/ObservedDayValueRepository.java
@@ -13,6 +13,18 @@ import ch.yoursource.causationfinder.entity.User;
 public interface ObservedDayValueRepository extends JpaRepository<ObservedDayValue, Integer>{
 
     // find all observed values from a given user on a given date
-    @Query("SELECT o FROM ObservedDayValue o LEFT JOIN CustomParameter c ON o.customParameter = c WHERE o.date = :date AND c.user = :user")
+    @Query("SELECT o FROM ObservedDayValue o INNER JOIN CustomParameter c ON o.customParameter = c WHERE o.date = :date AND c.user = :user")
     public List<ObservedDayValue> findByDateAndUser(@Param("date") Date date, @Param("user") User user);
+
+    // find all boolean and numeric observeday values where:
+    // customparameters are currently set active=true
+    // within range of a given start-and end-date
+    // get min. and max-value
+    // from a given user
+    // SELECT o.* FROM ObservedDayValue o INNER JOIN CustomParameter c ON o.custom_parameter_id = c.id
+    @Query("SELECT o, c FROM ObservedDayValue o INNER JOIN CustomParameter c ON o.customParameter = c "
+            + "WHERE o.date BETWEEN :startDate AND :endDate "
+            + "AND c.user = :user "
+            + "AND c.active = true")
+    public List<ObservedDayValue> findActiveByUserInRange(@Param("startDate") Date startDate, @Param("endDate") Date endDate, @Param("user") User user); 
 }

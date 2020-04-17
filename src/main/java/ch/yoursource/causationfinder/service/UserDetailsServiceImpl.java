0e9292bbefcacdf920f328d939ea9eb60bbf19d6
Melanie Fuchs <mele.fuchs@gmx.ch>
2020-04-17 12:36:58 +0200

    prevent disabled users from loggin in

--- src/main/java/ch/yoursource/causationfinder/service/UserDetailsServiceImpl.java
+++ src/main/java/ch/yoursource/causationfinder/service/UserDetailsServiceImpl.java
@@ -2,21 +2,22 @@ package ch.yoursource.causationfinder.service;
 
 
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
-import ch.yoursource.causationfinder.entity.User;
 import ch.yoursource.causationfinder.entity.Role;
+import ch.yoursource.causationfinder.entity.User;
 import ch.yoursource.causationfinder.repository.UserRepository;
-
-import java.util.HashSet;
-import java.util.Set;
 
 /*
  * The class is used to return an instance of spring's user (as our user is 
@@ -34,6 +35,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 	public UserDetails loadUserByUsername(String username) {
 		User user = userRepository.findByUsername(username);
         if (user == null) throw new UsernameNotFoundException(username);
+        if (!user.isEnabled()) throw new DisabledException("User disabled. Please activate account first");
 
         Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
         for (Role role : user.getRoles()){

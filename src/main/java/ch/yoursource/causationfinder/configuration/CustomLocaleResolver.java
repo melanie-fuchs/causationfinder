4aab50e72f4936de261a817af3b4a2be77a7e63a
Melanie Fuchs <mele.fuchs@gmx.ch>
2020-04-19 11:32:50 +0200

    set path for after logging out + fixed imports

--- src/main/java/ch/yoursource/causationfinder/configuration/CustomLocaleResolver.java
+++ src/main/java/ch/yoursource/causationfinder/configuration/CustomLocaleResolver.java
@@ -6,9 +6,6 @@ import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 

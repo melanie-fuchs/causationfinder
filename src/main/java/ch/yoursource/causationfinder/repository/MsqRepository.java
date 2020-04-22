616844aafa5afdc843d2f32340a61b98b4bc1c81
Melanie Fuchs <mele.fuchs@gmx.ch>
2020-04-22 15:29:37 +0200

    method to get all msq from a given user and a given date-span

--- src/main/java/ch/yoursource/causationfinder/repository/MsqRepository.java
+++ src/main/java/ch/yoursource/causationfinder/repository/MsqRepository.java
@@ -1,6 +1,7 @@
 package ch.yoursource.causationfinder.repository;
 
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -14,4 +15,8 @@ public interface MsqRepository extends JpaRepository<MedicalSymptomsQuestionnair
     // find by username and date
     @Query("SELECT m FROM MedicalSymptomsQuestionnaire m WHERE m.user = :user AND m.date = :date")
     public MedicalSymptomsQuestionnaire findByUserAndDate(@Param("user") User user, @Param("date") Date date);
+
+    // find all msq form a given user in a given date-range
+    @Query("SELECT m FROM MedicalSymptomsQuestionnaire m WHERE m.user = :user AND m.date BETWEEN :startDate AND :endDate")
+    public List<MedicalSymptomsQuestionnaire> findByUserInRange(@Param("user") User user, @Param("startDate") Date startDate, @Param("endDate") Date endDate);
 }

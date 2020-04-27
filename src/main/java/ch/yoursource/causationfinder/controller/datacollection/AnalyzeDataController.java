4f75f63d429b451d371f76abee0ef3e893828354
Melanie Fuchs <mele.fuchs@gmx.ch>
2020-04-27 11:09:14 +0200

    imports fixed

--- src/main/java/ch/yoursource/causationfinder/controller/datacollection/AnalyzeDataController.java
+++ src/main/java/ch/yoursource/causationfinder/controller/datacollection/AnalyzeDataController.java
@@ -14,7 +14,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -24,7 +23,6 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.ModelAndView;
 
